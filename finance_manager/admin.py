4bd74288e437b1dd90ea23c1e0c8a85c231e3e1b
Ameer Azlan Shah <2774679a@student.gla.ac.uk>
2024-03-15 10:26:22 +0000

    Reworked models and admin

--- finance_manager/admin.py
+++ finance_manager/admin.py
@@ -1,7 +1,27 @@
 from django.contrib import admin
-from finance_manager.models import FinancialAccount
-    
+from .models import FinancialAccount, Budget, Income, Expense, UserProfile
+
+# Register your models here.
+
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'slug')
+    prepopulated_fields = {'slug':('user',)}
+
 class FinancialAccountAdmin(admin.ModelAdmin):
-    account_details = []
-    
+    list_display = ('financial_account_name', 'username', 'financial_account_id', 'savings_balance', 'current_balance')
+    prepopulated_fields = {'slug':('username','financial_account_name',)}
+
+class BudgetAdmin(admin.ModelAdmin):
+    list_display = ('date', 'financial_account', 'category', 'price')
+
+class IncomeAdmin(admin.ModelAdmin):
+    list_display = ('date', 'financial_account', 'source', 'amount')
+
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ('date', 'financial_account', 'category', 'product_name', 'price')
+
+admin.site.register(UserProfile, UserProfileAdmin)
 admin.site.register(FinancialAccount, FinancialAccountAdmin)
+admin.site.register(Budget, BudgetAdmin)
+admin.site.register(Income, IncomeAdmin)
+admin.site.register(Expense, ExpenseAdmin)
