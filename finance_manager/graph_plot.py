cd9ada59e0d0b521e8810df75d34bef6c0cb7439
Robbie Copeland <2768763c@student.gla.ac.uk>
2024-03-16 21:08:08 +0000

    updated sample graph(untested)

--- finance_manager/graph_plot.py
+++ finance_manager/graph_plot.py
@@ -1,16 +1,18 @@
 from django import HttpResponse
 import pydot
-import io
 
 def create_graph(request):
-    sample_data = {'A' : 10, 'B' : 20, 'C' : 30, 'D' : 40}
+    sample_data = {'A': {'size': 30, 'color': 'red'},
+        'B': {'size': 40, 'color': 'blue'},
+        'C': {'size': 20, 'color': 'green'},
+        'D': {'size': 10, 'color': 'yellow'}}
 
     graph = pydot.Dot(graph_type='graph')
 
-    for label, sizes in sample_data.items():
-        node = pydot.Node(label, shape='circle', width=str(10))
+    for label, node_data in sample_data.items():
+        node = pydot.Node(label, shape='circle', width=str(node_data[10]), style='filled', fillcolor=node_data['color'])
         node.add_node(node)
 
-    image_data = graph.create_png(prog='spendings')
+    image_data = graph.create_png(prog='circo')
 
     return HttpResponse(image_data, content_type='image/png')
